0565ccfff38d220135b8a1563308c47ae93e077c
nadatani <nadaryu@gmail.com>
2017-12-28 13:51:44 +0900

    共有始める

---
+++ DropHole1/Assets/Scripts/CharaManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaManager : MonoBehaviour {
+
+
+    public float speed = 10;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rig = GetComponent<Rigidbody>();
+
+        rig.AddForce(x * speed , 0, z * speed);
+
+		
+	}
+}
